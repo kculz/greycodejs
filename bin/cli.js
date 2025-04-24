cb1239ca8f0767c341abaa28e4f7f76b17e7895d
kculz <munyamakudzai095@gmail.com>
2025-04-24 23:36:47 +0200

    FEAT: make-service <name> command

--- bin/cli.js
+++ bin/cli.js
@@ -742,6 +742,35 @@ program
     }
   });
 
+
+  program
+  .command('make-service <name>')
+  .description('Create a new service layer file')
+  .action((name) => {
+    const serviceName = `${name.charAt(0).toUpperCase() + name.slice(1)}Service`;
+    const serviceContent = `const { ${serviceName.replace('Service', '')} } = require('../models');
+
+class ${serviceName} {
+  static async getAll() {
+    return await ${serviceName.replace('Service', '')}.findAll();
+  }
+
+  // Add other service methods here
+}
+
+module.exports = ${serviceName};`;
+    
+    const servicesPath = path.resolve(process.cwd(), 'services');
+    const filePath = path.join(servicesPath, `${serviceName}.js`);
+    
+    if (!fs.existsSync(servicesPath)) {
+      fs.mkdirSync(servicesPath, { recursive: true });
+    }
+    
+    fs.writeFileSync(filePath, serviceContent);
+    console.log(chalk.green(`Service created at ${filePath}`));
+  });
+
 // List all commands
 program
   .command('list-commands')
